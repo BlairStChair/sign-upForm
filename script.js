a9073e479c00b483ce1f6e9d9380562744147e81
BlairStChair <julia.z15@interia.pl>
2024-11-08 16:58:58 +0100

    Fixed password validation

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const password = document.querySelector("#password")
 const confirmPassword = document.querySelector("#confirmPassword")
 const passwordFieldset = document.querySelector("#passwordFieldset");
 
-const passwordValue = password.value;
-const confirmPasswordValue = confirmPassword.value;
-
 const invalidPassword = document.createElement("p");
 invalidPassword.textContent = "Passwords are not the same!"
 invalidPassword.style.fontSize = "16px";
 invalidPassword.style.color = "red";
 
 createAccount.addEventListener("click", () =>{
+    const passwordValue = password.value;
+    const confirmPasswordValue = confirmPassword.value;
+
     if(passwordValue != confirmPasswordValue){
         password.style.borderWidth = "0.15vw";
         password.style.borderColor = "red";
@@ -19,5 +19,11 @@ createAccount.addEventListener("click", () =>{
         confirmPassword.style.borderColor = "red";
 
         passwordFieldset.appendChild(invalidPassword);
+    }else{
+        password.style.borderColor = "";
+        confirmPassword.style.borderColor = "";
+        if (passwordFieldset.contains(invalidPassword)) {
+            passwordFieldset.removeChild(invalidPassword);
+        }
     }
 });
